53563238f5effbcbdfe37a58b1c579b27bdba8ff
P-da <sergey.viktorych+phantomcity@gmail.com>
2019-03-03 16:35:44 +0300

    +Share action

--- app/src/main/java/com/hfad/pizzas/OrderActivity.java
+++ app/src/main/java/com/hfad/pizzas/OrderActivity.java
@@ -1,7 +1,9 @@
 package com.hfad.pizzas;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+
+        setSupportActionBar(  (Toolbar) findViewById(R.id.toolbar)  );
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
